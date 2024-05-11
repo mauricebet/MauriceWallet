1ce570620374317a97e009532d18fb7e500d5a85
Nicholas Abraham <z5438689@ad.unsw.edu.au>
2024-05-11 18:59:56 +1000

    added stub functions for wallet file

--- wallet.ts
+++ wallet.ts
@@ -1,1 +1,39 @@
 // This manages all wallets functions
+
+/**
+ * Returns the current balance of the user's wallet
+ * 
+ * @param {string} userName     - Username of the wallet owner
+ * 
+ * @return {number}             - Balance value
+ */
+export function checkBalance(username: string): number {
+
+    return 0;
+}
+
+/**
+ * Sends money to target receiver
+ * 
+ * @param {string} receiver     - receiver's userName
+ * @param {string} sender       - sender's username
+ * @param {number} ammount      - Ammount of money to be transferred
+ * 
+ * @return {boolean}            - True if transaction was successful
+ */
+export function transfer(receiver: string, sender: string, ammount: number): boolean {
+    return false;
+}
+
+/**
+ * Adds value to balance
+ * 
+ * @param {string} userName     - Username
+ * @param {number} ammount      - Ammount to be topped up
+ * 
+ * @return {boolean}            - True if successfully topped up
+ */
+export function topUp(userName: string, ammount: number): boolean {
+    return false;
+}
+
